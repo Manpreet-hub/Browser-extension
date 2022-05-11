9bc5119e58f521bb464cbbb042fa128042afdf73
Manpreet <manpreetisar@gmail.com>
2022-05-11 17:59:43 +0530

    add react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import { Onboarding, UserInfo } from "./pages/";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Onboarding />} />
+        <Route path="/info" element={<UserInfo />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
